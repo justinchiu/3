6f66c551939a1bc05c657e7f9edf4395cb8dde8d
Justin Chiu <justinchiu@g.harvard.edu>
2018-07-10 16:13:16 -0400

    star with wt 2

--- scripts/get_wikitext.sh
+++ scripts/get_wikitext.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p data
 pushd data
-curl -O https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
-unzip wikitext-103-v1.zip
+#curl -O https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
+#unzip wikitext-103-v1.zip
+curl -O https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
+unzip wikitext-2-v1.zip
 popd
