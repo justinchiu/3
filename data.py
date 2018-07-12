d8aaa20342f97f8cadbc9ec28fba388daa0077c5
Justin Chiu <justinchiu@g.harvard.edu>
2018-07-12 16:56:11 -0400

    switch to ptb for now for vae

---
+++ data.py
@@ -0,0 +1,33 @@
+from torchtext import data
+from torchtext import datasets
+import io
+
+class StructuredLmDataset(data.Dataset):
+    def __init__(
+        self, path, text_field, newline_eos=True,
+        encoding='utf-8', **kwargs,
+    ):
+        """Create a LanguageModelingDataset given a path and a field.
+        Arguments:
+            path: Path to the data file.
+            text_field: The field that will be used for text data.
+            newline_eos: Whether to add an <eos> token for every newline in the
+                data file. Default: True.
+            Remaining keyword arguments: Passed to the constructor of
+                data.Dataset.
+        """
+        fields = [('text', text_field)]
+        text = []
+        with io.open(path, encoding=encoding) as f:
+            for line in f:
+                pline = text_field.preprocess(line)
+                import pdb; pdb.set_trace()
+                text += pline
+                if newline_eos:
+                    text.append(u'<eos>')
+
+        examples = [data.Example.fromlist([text], fields)]
+        super(StructuredLmDataset, self).__init__(
+            examples, fields, **kwargs)
+
+
--- train.py
+++ train.py
@@ -2,18 +2,31 @@ import argparse
 
 from torchtext import data
 from torchtext import datasets
+from data import StructuredLmDataset
 
 def get_args():
     args = argparse.ArgumentParser()
-    args.add_argument()
-    return args.get_args()
+    args.add_argument("-devid", type=int, default=-1)
+    args.add_argument("-bsz", type=int, default=1)
+    args.add_argument("-bptt", type=int, default=50)
+    return args.parse_args()
+args = get_args()
 
+device = "cpu" if args.devid < 0 else "cuda:{}".format(args.devid)
+
+#TEXT = data.Field(lower=True, batch_first=False)
 TEXT = data.Field(lower=True, batch_first=True)
+"""
+train, valid, test = StructuredLmDataset.splits(
+    path="data/wikitext-2",
+    train="wiki.train.tokens",
+    validation="wiki.valid.tokens",
+    test="wiki.test.tokens",
+    text_field=TEXT)
+"""
+train, valid, test = datasets.PennTreebank.splits(text_field=TEXT)
 TEXT.build_vocab(train)
 
 train_iter, valid_iter, test_iter = data.BPTTIterator.splits(
-    (train, valid, test), batch_size=3, bptt_len=30, device="cuda:0")
-
-train_iter, valid_iter, test_iter = datasets.WikiText2.iters(
-    batch_size=4, bptt_len=30)
+    (train, valid, test), batch_size=args.bsz, bptt_len=args.bptt, device=device)
 
