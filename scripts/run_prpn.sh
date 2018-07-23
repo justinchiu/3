21930e27e4437783adbe449f7a17f1c5486ebb1f
Justin Chiu <justinchiu@g.harvard.edu>
2018-07-23 14:13:04 -0400

    run shuffled

--- scripts/run_prpn.sh
+++ scripts/run_prpn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m models.prpn.main_LM --cuda --data .data/penn-treebank/ptb. > prpn_ref.log
+CUDA_VISIBLE_DEVICES=3 python -m models.prpn.main_LM --cuda --data .data/penn-treebank/ptb. > prpn_ref.log
