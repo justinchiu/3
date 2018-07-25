1e5f187e8d2803a78dd520dc6b3e6559285d9db3
Justin Chiu <justinchiu@g.harvard.edu>
2018-07-25 19:26:31 -0400

    re-add shuffle?

--- scripts/run_prpn.sh
+++ scripts/run_prpn.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=3 python -m models.prpn.main_LM --cuda --data .data/penn-treebank/ptb. > prpn_ref.log
+#CUDA_VISIBLE_DEVICES=3 python -m models.prpn.main_LM --cuda --data .data/penn-treebank/ptb. --tied --hard > prpn_ref_noshuffle.log
+CUDA_VISIBLE_DEVICES=3 python -m models.prpn.main_LM --cuda --data .data/penn-treebank/ptb. --tied --hard > prpn_ref_shuffle.log
